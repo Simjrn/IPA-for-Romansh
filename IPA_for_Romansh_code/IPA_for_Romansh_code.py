750e1e25744728c70a600bc9a5126593d099b025
Simjrn <simonjenni374@gmail.com>
2025-08-01 19:12:20 +0100

    Update IPA_for_Romansh_code.py

--- IPA_for_Romansh_code/IPA_for_Romansh_code.py
+++ IPA_for_Romansh_code/IPA_for_Romansh_code.py
@@ -5,14 +5,33 @@ finished = []
 while True:
   letter = word[0]
 for index in range(len(word)):
-  if letter == c:
+  if letter == "c":
     if word[1] == "e" or "i":
       finished.append('ts')
+      word = word.lstrip("c")
     else:
       finish.append('k')
-  elif letter == j:
+      word = word.lstrip("c")
+  elif letter == "j":
     finished.append('y')
-  elif letter == a:
-    if word[1] == u:
+    word = word.lstrip("j")
+  elif letter == "a":
+    if word[1] == "u":
       finished.append('a')
       finished.append('o')
+      word = word.lstrip("a")
+      word = word.lstrip("u")
+  elif letter == "t":
+    if word[1] == "g":
+      finished.append('c')
+      finished.append('i')
+      word = word.lstrip("t")
+      word = word.lstrip("g")
+    else:
+      finished.append('t')
+      word = word.lstrip("t")
+  else:
+    finished.append(letter)
+    word = word.lstrip(letter)
+result = "".join(finished)
+st.subtittle(result)
