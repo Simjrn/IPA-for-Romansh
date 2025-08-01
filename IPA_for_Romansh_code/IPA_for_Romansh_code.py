b8201b7cd1fbd45227866f4b90127171312650e5
Simjrn <simonjenni374@gmail.com>
2025-08-01 18:59:17 +0100

    Create IPA_for_Romansh_code.py

---
+++ IPA_for_Romansh_code/IPA_for_Romansh_code.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+word = st.text_input("Write your word here")
+finished = []
+while True:
+  letter = word[1]
+for index in range(len(word)):
+  if letter == c:
+    if word[index + 1] == "e" or "i":
+      finished.append('ts')
+    else:
+      finish.append('k')
+  elif letter == j:
+    finished.append('y')
+  elif letter == a:
+    if word[index + 1] == u:
+      finished.append('a')
+      finished.append('o')
