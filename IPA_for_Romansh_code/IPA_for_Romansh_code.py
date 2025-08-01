4dd9d389aeb0abea8a2fc3f1eaeeebdd669f1bb8
Simjrn <simonjenni374@gmail.com>
2025-08-01 19:00:38 +0100

    Update IPA_for_Romansh_code.py

--- IPA_for_Romansh_code/IPA_for_Romansh_code.py
+++ IPA_for_Romansh_code/IPA_for_Romansh_code.py
@@ -3,16 +3,16 @@ import streamlit as st
 word = st.text_input("Write your word here")
 finished = []
 while True:
-  letter = word[1]
+  letter = word[0]
 for index in range(len(word)):
   if letter == c:
-    if word[index + 1] == "e" or "i":
+    if word[1] == "e" or "i":
       finished.append('ts')
     else:
       finish.append('k')
   elif letter == j:
     finished.append('y')
   elif letter == a:
-    if word[index + 1] == u:
+    if word[1] == u:
       finished.append('a')
       finished.append('o')
